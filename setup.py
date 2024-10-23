5d29a46ed9c8662cffe83dda362ae83312af8a2f
Minghao Li <minghaoli@google.com>
2024-10-23 17:53:38 +0800

    Fix wrong import and dump latest code

--- setup.py
+++ setup.py
@@ -16,6 +16,11 @@
 
 import setuptools
 
+description = (
+    'Mobly WiFi controller module for using Python code to operate network '
+    'devices in Mobly tests.'
+)
+
 install_requires = [
     'mobly>=1.12.2',
     'immutabledict',
@@ -26,16 +31,16 @@ install_requires = [
 
 setuptools.setup(
     name='mobly-wifi',
-    version='1.1b1',
+    version='1.1b2',
     author='Minghao Li',
     author_email='minghaoli@google.com',
-    description='Mobly WiFi controller module for using Python code to operate network devices in Mobly tests.',
+    description=description,
     license='Apache2.0',
     url='https://github.com/google/mobly-wifi',
-
-    packages=['mobly.controllers.wifi'],
+    packages=setuptools.find_namespace_packages(
+        include=['mobly.controllers.*']
+    ),
     package_data={'mobly.controllers.wifi': ['data/*']},
-
     install_requires=install_requires,
     python_requires='>=3.11',
 )
